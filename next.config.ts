91cdfef67c231b47647b18b9f123059f4c53ea6b
Greg LaPoint <greg@ghsedge.com>
2025-12-16 11:43:32 -0600

    Add Netlify configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProduction ? '/edge-team-showcase' : '',
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProduction ? '/edge-team-showcase' : '',
   },
 };
 
